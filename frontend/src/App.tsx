2be41a7f25779946265cb7de11ac7409e42230b0
Abhishek Kakolla <abhishekkakolla1@gmail.com>
2025-03-01 16:41:12 -0800

    added hyperparameter tuning in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+
+// Web socket function
 function run(setWs: React.Dispatch<React.SetStateAction<WebSocket | null>>, setMsg: React.Dispatch<React.SetStateAction<string | undefined>>) {
   console.log("starting");
   const socket: WebSocket = new WebSocket("ws://localhost:5000");
   setWs(socket);
 
+  // first send hyperparameters for the C++ program
+  const pop_size: number = parseFloat((document.getElementById("pop_size") as HTMLInputElement).value) || 200;
+  const select_rate: number = parseFloat((document.getElementById("select_rate") as HTMLInputElement).value) || 0.10;
+  const mut_rate: number = parseFloat((document.getElementById("mut_rate") as HTMLInputElement).value) || 0.05;
+  console.log(pop_size);
+  console.log(select_rate);
+  console.log(mut_rate);
+  const hyperparameters = [pop_size, select_rate, mut_rate];
+
+
   socket.onopen = () => {
     console.log("Connected to WebSocket server");
-    socket.send("Start algorithm"); // Send message to start C++ process
+    socket.send(JSON.stringify(hyperparameters)); // Send message to start C++ process
+    // socket.send("Start");
   };
 
   // receiving msgs
@@ -31,20 +44,21 @@ function App() {
   const [msg, setMsg] = useState<string>();
   const [ws, setWs] = useState<WebSocket | null>(null);
 
-  // useEffect(() => {
-
-  // }, []);
 
   return (
     <div>
       <h1>Genetic Algorithm</h1>
+      <div>
+        <h4>Population size</h4>
+        <input type="number" id="pop_size" defaultValue={200}></input>
+        <h4>Selection rate</h4>
+        <input type="number" id="select_rate" defaultValue={0.1}></input>
+        <h4>Mutation rate</h4>
+        <input type="number" id="mut_rate" defaultValue={0.05}></input>
+      </div>
+      <br />
       <button onClick={() => run(setWs, setMsg)}>Run Algorithm</button>
       <h2>{msg}</h2>
-      {/* <ul>
-        {msgs.map((msg, i) => (
-          <li key={i}>{msg}</li>
-        ))}
-      </ul> */}
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -11,8 +11,19 @@ wss.on('connection', (ws) => {
     ws.on('message', (msg) => {
         console.log('received: ', msg);
 
+        // first initialize parameters
+        const receivedHyperparameters = JSON.parse(msg);
+        const file_name = "target.txt";
+        console.log("using these parameters: ", receivedHyperparameters)
+        let pop_size;
+        let select_rate;
+        let mut_rate;
+
+        [pop_size, select_rate, mut_rate] = receivedHyperparameters;
+
         // run the c++ algo
-        const cppProcess = spawn('../program.exe', ["target.txt", 200, 0.1, 0.05]);
+        const cppProcess = spawn('../program.exe', [file_name, pop_size, select_rate, mut_rate]);
+        // const cppProcess = spawn('../program.exe', ["target.txt", 200, 0.1, 0.05]);
 
         // send each iterations output from stdout
         cppProcess.stdout.on('data', (data) => {
